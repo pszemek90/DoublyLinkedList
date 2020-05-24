a775660c6a062bd47720e1ad6abbf0b3bc756de6
pszemek90 <me@example.com>
2020-05-24 20:41:02 +0200

    Doubly Linked list first commit

---
+++ StringListElement.java
@@ -0,0 +1,40 @@
+package sda._200523.TwoDirectionalList;
+
+public class StringListElement {
+    private String value;
+    private StringListElement nextElement;
+    private StringListElement previousElement;
+
+
+    public StringListElement() {
+
+    }
+
+    public StringListElement(String value) {
+        this.value = value;
+    }
+
+    public StringListElement getPreviousElement() {
+        return previousElement;
+    }
+
+    public void setPreviousElement(StringListElement previousElement) {
+        this.previousElement = previousElement;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public StringListElement getNextElement() {
+        return nextElement;
+    }
+
+    public void setNextElement(StringListElement nextElement) {
+        this.nextElement = nextElement;
+    }
+}
